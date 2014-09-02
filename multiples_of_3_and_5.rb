25954ea7fcb4f0ba2e785edbb3c74b3fe5632e85
stbarrientos <stbarrientos@gmail.com>
2014-09-02 09:19:55 -0400

    solved problem 1

---
+++ multiples_of_3_and_5.rb
@@ -0,0 +1,19 @@
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+array = []
+
+1.upto(999) do |x|
+
+	if x % 3 && x % 5 == 0
+		array << x
+	elsif x % 3 == 0
+		array << x
+	elsif x % 5 == 0
+		array << x
+	end
+end
+
+array.compact!
+puts array.inject(:+)
